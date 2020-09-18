ab198cdd1abb09fc41c8382bd194085ea54a95b6
CelesteMagisteel <kamg1134@gmail.com>
2020-09-18 14:22:31 +0100

    Simplified TileMap class

--- src/main/java/net/celestemagisteel/map/TileMap.java
+++ src/main/java/net/celestemagisteel/map/TileMap.java
@@ -14,30 +14,14 @@ public class TileMap implements Serializable {
         this.map = map;
     }
 
-    public TileMap() {
-
-    }
+    public TileMap() { }
 
     public Tile[][] getMap() { return map; }
 
     public Tile getTile(int x, int y) { return map[y][x]; }
 
-    public void printTileMap() {
-        for (int y = 0; y < MAP_SIZE; y++) {
-            for (int x = 0; x < MAP_SIZE; x ++) {
-                if (map[y][x] == null) {
-                    System.out.print(" ");
-                } else {
-                    System.out.print(map[y][x].getShortCode());
-                }
-            }
-            System.out.println();
-        }
-    }
-
     public static TileMap generateBasicTileMap() {
         Tile[][] map = new Tile[MAP_SIZE][MAP_SIZE];
-        Random random = new Random();
 
         // Outline
         for (int i = 0; i < MAP_SIZE; i++) {
@@ -48,7 +32,6 @@ public class TileMap implements Serializable {
             }
             map[MAP_SIZE-1][i] = TileFactory.generateNewFloorTile();
         }
-
 
         return new TileMap(map);
     }
