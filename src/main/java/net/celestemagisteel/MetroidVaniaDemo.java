13a178f151fa7a8776f643ad3d6f86e541784da1
CelesteMagisteel <kamg1134@gmail.com>
2020-09-18 12:24:41 +0100

    Switched to using JFX8 instead of LWJGL

--- src/main/java/net/celestemagisteel/MetroidVaniaDemo.java
+++ src/main/java/net/celestemagisteel/MetroidVaniaDemo.java
@@ -1,12 +1,19 @@
 package net.celestemagisteel;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import net.celestemagisteel.map.TileMap;
 
-public class MetroidVaniaDemo {
+public class MetroidVaniaDemo extends Application {
 
     public static void main(String[] args) {
         TileMap map = TileMap.generateBasicTileMap();
 
         map.printTileMap();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }
